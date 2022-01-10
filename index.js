e9e9dd56f4485cdda22e492bc55a1b0fec649832
Macbook <tahreer.90z@gmail.com>
2022-01-10 17:31:29 +0300

    Arrays completed

---
+++ index.js
@@ -0,0 +1,18 @@
+let emptyArray = []
+
+let hobbies = [
+    "baking", "Coding", "Cooking", "Reading", "Swimming", "Painting"
+]
+let numbers = [1,4,5,6,3]
+console.log(numbers [1])
+console.log(hobbies[hobbies.length -1])
+hobbies.push("Writing")
+console.log(hobbies[6])
+console.log(hobbies.length)
+console.log(hobbies.pop())
+console.log(hobbies.pop())
+console.log(hobbies.length)
+console.log(hobbies)
+hobbies = []
+
+
